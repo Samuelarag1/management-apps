0d3c3376638f9408a2fd4951720428abb8da6726
Samuel <samuelaragon616@gmail.com>
2025-05-28 15:39:10 -0300

    update details stetic

--- components/dashboard-header.tsx
+++ components/dashboard-header.tsx
@@ -13,15 +13,18 @@ import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 export function DashboardHeader() {
   return (
-    <header className="sticky top-0 z-30 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6 justify-between">
-      <Link href={"/"} className="font-bold md:block md:text-2xl">
+    <header className="text-white bg-[#7c3aed] sticky top-0 z-30 flex h-16 items-center gap-4 border-b px-4 md:px-6 justify-between">
+      <Link
+        href={"/"}
+        className="font-black md:block md:text-3xl text-2xl text-shadow"
+      >
         Administra
       </Link>
 
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant="outline" size="icon" className="shrink-0">
-            <User className="h-5 w-5" />
+            <User className="h-5 w-5" color="black" />
             <span className="sr-only">Perfil</span>
           </Button>
         </DropdownMenuTrigger>
